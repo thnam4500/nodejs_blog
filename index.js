5b5d14d8475673f963bd02b8e3757acfc9ffb307
Hoang Nam <thnam4500@gmail.com>
2022-02-24 15:57:56 +0700

    initialize commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    var a =1;
+    var b = 2;
+    var c =  a+b;
+  res.send("Hello World!");
+});
+
+app.get("/tin-tuc",(req, res)=>{
+    res.send("<h1>TIN TUC</h1>")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
